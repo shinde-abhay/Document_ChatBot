66318a0a35e8bdc71af9e22b9b3b2bd0ec8e316f
Abhay Shinde <abhayshinde600@gmail.com>
2026-08-26 19:34:56 +0530

    Use environment variable for API URL

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
-
-API_URL = "http://127.0.0.1:8000"
+import os
+API_URL = os.getenv("BACKEND_URL", "http://127.0.0.1:8000")
 
 st.title("Document Chatbot")
 
